c923b356240610ee6af63f7f525d7d4992209a17
Claire Curry <curryclairem@gmail.com>
2024-05-28 17:32:33 -0500

    testing date formats with new files

--- testing_geolocator.R
+++ testing_geolocator.R
@@ -14,16 +14,20 @@ tbl <- read.csv("C:/Users/curr0024/Downloads/BC089_23Feb18_233911.lux",
                 header = FALSE,
                 sep="\t", #.lux is tab separated not comma
                 skip = 20)
+#testing DM file from issue 6 2023/09/28
+tbl <- read.csv("C:/Users/curr0024/Desktop/temporary/ID000418_2023Mar24T163221.csv",
+               header = TRUE)
 
 #names headers
 names(tbl) <- c("datetime", "light")
 #specifies date and time format.
-tbl$datetime <- as.POSIXct(strptime(tbl$datetime,
-                                    format = "%d/%m/%Y %H:%M:%S",
-                                    tz = "GMT"))
+tbl$datetime <- as.POSIXct(x=tbl$datetime,
+                           tz = "GMT",
+                           tryFormats = c("%d/%m/%Y %H:%M:%S",
+                                          "%Y/%m/%d %H:%M:%S"))
 
 
-light_threshold <- 5.5
+LightThreshold <- TRUE
 problem_threshold <- 5
 
 source("TAGS_shiny/source_TAGS_twilightCalc.R") #TAGS_twilight_calc
@@ -31,6 +35,38 @@ twl <-   TAGS_twilight_calc(tbl$datetime,
                      tbl$light, 
                      LightThreshold = light_threshold,
                      allTwilights = TRUE)
+
+
+datetime <- tbl$datetime
+light <- tbl$light
+dt <- cut.POSIXt(datetime,"1 hour")
+st <- as.POSIXct(levels(dt),"UTC")
+
+raw <- data.frame(datetime=dt,light=light)
+
+h  <- tapply(light,dt,max)
+df1 <- data.frame(datetime=st+(30*60),light=as.numeric(h))
+
+smooth <- i.twilightEvents(df1[,1], df1[,2], LightThreshold)
+smooth <- data.frame(id=1:nrow(smooth),smooth)
+raw    <- i.twilightEvents(datetime, light, LightThreshold)
+raw <- data.frame(id=1:nrow(raw),raw)
+
+ind2 <- rep(NA,nrow(smooth))
+for(i in 1:nrow(smooth)){
+  tmp <- subset(raw,datetime>=(smooth[i,2]-(90*60)) & datetime<=(smooth[i,2]+(90*60)))
+  
+  if(smooth[i,3]==1) ind3 <- tmp$id[which.min(tmp[,2])]
+  if(smooth[i,3]==2) ind3 <- tmp$id[which.max(tmp[,2])]
+  ind2[i] <- ind3
+}
+
+
+res <- data.frame(raw,mod=1)
+res$mod[ind2] <- 0
+
+
+
 
 
 
@@ -87,9 +123,10 @@ plotall <- ggplot() +
 
 
 #.lig version
-tbl <- read.csv("data/PABU222150719.lig",
+tbl <- read.csv("data/GL36_000.lig",
                 header = FALSE,
-                sep=",")
+                sep=",",
+                skip = 1)
 #specifies date and time format.
 tbl$datetime <- as.POSIXct(strptime(tbl$V2,
                                     format = "%d/%m/%y %H:%M:%S",
